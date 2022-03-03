90fbff9916fd596595c0a42e468324d0b0986f17
kelvinchanwh <kelvin.chanwh@hotmail.com>
2022-03-03 19:04:27 +0800

    Added ptc_optimization

--- optimize.py
+++ optimize.py
@@ -3,19 +3,19 @@ import numpy as np
 import pandas as pd
 import os
 import model
+import scipy.optimize as opt
 
 class optimizer():
     def __init__(self, model, max_k = 100, max_line = 100, max_j = 100, opt_tolerance = 0.01):
         self.k = 0
         self.j = 0
-        self.x_kj = np.zeros(self.k, self.j)
+        self.x_kj = model
         self.x_kj_conv = self.x_kj
         self.x_kj_init = self.x_kj
         self.max_k = max_k
         self.max_line = max_line
         self.max_j = max_j
         self.opt_tolerance = opt_tolerance
-        self.model = model
 
 
     # def initial_values(self):
@@ -46,7 +46,7 @@ class optimizer():
         return False
 
     def steady_state_sim(self):
-        self.model.run()
+        self.x_kj.run()
 
     def nlp_optimize(self):
         # newton
@@ -55,6 +55,26 @@ class optimizer():
 
     def convergence_test(self):
         return True
+
+    def ptc_objective(self):
+        sum_of_squares = 0
+        for j in range (self.x_kj.N):
+            for i in range (len(self.x_kj.components)):
+                dM_ij_dt =  self.x_kj.V(j+1) * self.x_kj.components[i].y(j+1) \
+                    + self.x_kj.L(j-1) * self.x_kj.components[i].x(j-1) \
+                        - self.x_kj.V_eq(j) * self.x_kj.components[i].y_eq(j) \
+                            - self.x_kj.L_eq(j) * self.x_kj.components[i].x_eq(j) 
+                sum_of_squares += dM_ij_dt**2
+            dH_j_dt =  self.x_kj.V(j+1) * self.x_kj.H(j+1) \
+                + self.x_kj.L(j-1) * self.x_kj.h(j-1) \
+                    - self.x_kj.V_eq(j) * self.x_kj.H_eq(j) \
+                        - self.x_kj.L_eq(j) * self.x_kj.h_eq(j) 
+            sum_of_squares += dH_j_dt**2
+        return sum_of_squares
+
+    def ptc_optimize(self):
+        x0 = self.x_kj
+        x_opt = opt.minimize(self.ptc_objective, x0, method='SLSQP', options={'disp': True})
 
     def main(self):
         while self.j < self.max_j:
