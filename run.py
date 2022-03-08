c3ebea624a92e2497076c07dcf675338ad1394a2
kelvinchanwh <kelvin.chanwh@hotmail.com>
2022-03-09 03:42:11 +0800

    Sectional Pressure Drop

--- run.py
+++ run.py
@@ -6,9 +6,11 @@ model = m.Model (
     components = ["BENZENE", "TOLUENE"], 
     N = 36,
     RR = 0.924,
-    feed_stage = 23, 
-    P_cond = 1.12, #bar 
-    P_drop = 0,
+    tray_spacing = 0.63636,
+    feed_stage = 23,
+    P_cond = 1.12,
+    P_drop_1 = 0.1, 
+    P_drop_2 = 0.2
 )
 
 model.run()
