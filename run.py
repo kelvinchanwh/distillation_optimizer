3f1dc4424e76caf40f643d3430480fca391c4c4a
kelvinchanwh <kelvin.chanwh@hotmail.com>
2022-03-16 02:58:17 +0800

    Removed components input

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import model as m
 
 model = m.Model ( 
     filepath = os.path.join(os.getcwd(), 'Simulation 2.bkp'), 
-    components = ["BENZENE", "TOLUENE"], 
     N = 36,
     RR = 0.924,
     tray_spacing = 0.63636,
