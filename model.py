aedb96efc45ba5bb4910451a7d93a8cb5050da8a
kelvinchanwh <kelvin.chanwh@hotmail.com>
2022-03-31 22:50:04 +0800

    Current pressure stage check should include >=

--- model.py
+++ model.py
@@ -128,7 +128,7 @@ class Model:
         self.setValue(r"\Data\Blocks\B1\Input\PDROP_SEC\2", self.P_drop_2)
         # If current 2nd start stage is smaller then upcoming 1st end stage, then set the upcoming 2nd start stage first
         curr2start = self.getValue(r"\Data\Blocks\B1\Input\PRES_STAGE1\2")
-        if (self.P_end_1 > curr2start):
+        if (self.P_end_1 >= curr2start):
             self.setValue(r"\Data\Blocks\B1\Input\PRES_STAGE1\2", self.P_start_2)
             self.setValue(r"\Data\Blocks\B1\Input\PRES_STAGE2\1", self.P_end_1)
         else:
