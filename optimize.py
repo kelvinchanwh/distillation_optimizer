6494c9ea343e155688ee188848cac0e45e2caf38
kelvinchanwh <kelvin.chanwh@hotmail.com>
2022-04-05 22:57:46 +0800

    Reduced number of variables

--- optimize.py
+++ optimize.py
@@ -167,18 +167,15 @@ class Optimizer():
 
     def callback(self, x):
         self.func_iter = 0
-        print ('{0:4d}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:3.3f}   {7:3.3f}   {8:3.3f}   {9:3.3f}'.format(self.opt_iter, x[0], x[1], x[2], x[3], x[4], x[5], x[6], self.model.TAC, self.time))
+        print ('{0:4d}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:3.3f}'.format(self.opt_iter, x[0], x[1], x[2], x[3], self.model.TAC, self.time))
         self.opt_iter += 1
 
     def optimize(self):
         x0 = [
-            self.model.P_cond, 
-            self.model.P_drop_1, 
-            self.model.P_drop_2, 
+            self.model.P_cond,
             initialize.min_RR(self.model), 
             initialize.actual_N(self.model, self.recoveryLB), 
-            initialize.feed_stage(self.model, self.recoveryLB), 
-            self.model.tray_spacing
+            initialize.feed_stage(self.model, self.recoveryLB)
             ]
         
         min_RR = initialize.min_RR(self.model, self.recoveryLB)
@@ -186,12 +183,9 @@ class Optimizer():
 
         bounds = (
             (1.013, 10), # P_cond
-            (0.01, 1.0), # P_drop_1
-            (0.01, 1.0), # P_drop_2
             (min_RR, 1.1 * min_RR), # RR
             (min_N, 300), # N
             (2, self.model.N-2), # feed_stage
-            (0.15, 1), # tray_spacing
         )
 
         constraints = (
@@ -213,8 +207,8 @@ class Optimizer():
             {'type': 'ineq', 'fun': self.entrainmentFracCheckBottom},
         )
 
-        print ('{0:4s}   {1:9s}   {2:9s}   {3:9s}   {4:9s}   {5:9s}   {6:9s}   {7:9s}   {8:9s}   {9:9s}'.format('Iter', ' P_cond', 'P_drop_1', 'P_drop_2', 'RR', 'N', 'feed_stage', 'tray_spacing', 'TAC', 'Runtime'))
-        print ('{0:4s}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:3.3f}   {7:3.3f}   {8:4s}   {9:3.3f}'.format("Init", x0[0], x0[1], x0[2], x0[3], x0[4], x0[5], x0[6], "----", self.time))
+        print ('{0:4s}   {1:9s}   {2:9s}   {3:9s}   {4:9s}   {5:9s}   {6:9s}'.format('Iter', ' P_cond', 'RR', 'N', 'feed_stage', 'TAC', 'Runtime'))
+        print ('{0:4s}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:4s}'.format("Init", x0[0], x0[1], x0[2], x0[3], "----", self.time))
         result = opt.minimize(
             self.objective,
             x0, 
@@ -230,12 +224,9 @@ class Optimizer():
     def objective(self, x):
         try:
             self.model.P_cond = float(x[0])
-            self.model.P_drop_1 = float(x[1])
-            self.model.P_drop_2 = float(x[2])
-            self.model.RR = float(x[3])
-            self.model.N = int(x[4])
-            self.model.feed_stage = int(x[5])
-            self.model.tray_spacing = float(x[6])
+            self.model.RR = float(x[1])
+            self.model.N = int(x[2])
+            self.model.feed_stage = int(x[3])
             runtime = self.model.run()
             self.time += runtime
             self.func_iter += 1
@@ -243,7 +234,7 @@ class Optimizer():
         except Exception as e:
             # If simulation cannot be run, return a large number
             self.time += runtime
-            print ('{0:4d}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:3.3f}   {7:3.3f}   {8:3.3f}   {9:3.3f}'.format(self.func_iter, x[0], x[1], x[2], x[3], x[4], x[5], x[6], e, self.time))
+            print ('{0:4d}   {1:3.3f}   {2:3.3f}   {3:3.3f}   {4:3.3f}   {5:3.3f}   {6:3.3f}'.format(self.func_iter, x[0], x[1], x[2], x[3], e, self.time))
             self.func_iter += 1
             return np.inf
 
--- run.py
+++ run.py
@@ -4,9 +4,11 @@ import optimize as opt
 
 model = m.Model ( 
     filepath = os.path.join(os.getcwd(), 'Simulation 2.bkp'), 
+    P_drop_1= 0.01,
+    P_drop_2= 0.01,
     N = 36,
     RR = 0.924,
-    tray_spacing = 0.63636,
+    tray_spacing = 0.15,
     feed_stage = 23,
     P_cond = 1.013,
     main_component = "BENZENE"
