ed1bd556c2d85b353d88975da6422b3c8e8afe29
Shanice Tan <shanice.tan@student.manchester.ac.uk>
2022-04-24 04:26:04 +0800

    Added simulator

--- run.py
+++ run.py
@@ -3,25 +3,43 @@ import model as m
 import optimize as opt
 
 model = m.Model ( 
-    filepath = os.path.join(os.getcwd(), 'Simulation 3.bkp'), 
+    filepath = os.path.join(os.getcwd(), 'Simulation 5.bkp'), 
     N = 101,
     RR = 0.924,
     tray_spacing = 0.63636,
     # Tray Type: SIEVE, CAPS
-    tray_type="SIEVE",
-    tray_eff_1 = 0.5,
-    tray_eff_2 = 0.5,
+    tray_type="CAPS",
+    tray_eff_1 = 1.0,
+    tray_eff_2 = 1.0,
     feed_stage = 51,
     P_cond = 1.013,
     main_component = "BENZENE",
-    hydraulics = True
+    hydraulics = False
 )
 
 model.run()
 print (model.TAC)
 
-# Optimize
-optimizer = opt.Optimizer(model, opt_tolerance=1e-3, recoveryLB=0.95, purityLB=0.95)
-print (optimizer.run())
 
-# model.obj.Close()
+import conversions
+
+model.update_manipulated(
+    P_cond = 1.013, 
+    P_drop_1 = 0.06,
+    P_drop_2 = 0.06,
+    RR = 0.9002, 
+    N = 50, 
+    feed_stage = 35, 
+    tray_spacing = 0.6364, 
+    tray_type = "SIEVE",
+    hydraulics = False
+)
+model.run()
+tac = model.TAC
+diameter = model.diameter
+Q_cond = conversions.calPerSec_to_kJPerSec(abs(model.Q_cond))
+Q_reb = conversions.calPerSec_to_kJPerSec(abs(model.Q_reb))
+energy_cost = model.calc_energy_cost("hp" if model.T_stage[-2] > 150 else "lp")
+capital_cost = (model.TAC - energy_cost)
+
+print ("TAC: %f, Diameter: %f, Q_cond: %f, Q_reb: %f, energy: %f, capital: %f"%(tac, diameter, Q_cond, Q_reb, energy_cost, capital_cost))
