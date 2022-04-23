905d8b7d1e937221902d529bbbb619b3dc89a19e
Shanice Tan <shanice.tan@student.manchester.ac.uk>
2022-04-24 05:04:18 +0800

    Changed Values

--- run.py
+++ run.py
@@ -25,14 +25,14 @@ import conversions
 
 model.update_manipulated(
     P_cond = 1.013, 
-    P_drop_1 = 0.06,
-    P_drop_2 = 0.06,
-    RR = 0.9002, 
-    N = 50, 
-    feed_stage = 35, 
+    P_drop_1 = 0.01,
+    P_drop_2 = 0.01,
+    RR = 0.9071, 
+    N = 47, 
+    feed_stage = 32, 
     tray_spacing = 0.6364, 
     tray_type = "SIEVE",
-    hydraulics = False
+    hydraulics = True
 )
 model.run()
 tac = model.TAC
@@ -43,3 +43,6 @@ energy_cost = model.calc_energy_cost("hp" if model.T_stage[-2] > 150 else "lp")
 capital_cost = (model.TAC - energy_cost)
 
 print ("TAC: %f, Diameter: %f, Q_cond: %f, Q_reb: %f, energy: %f, capital: %f"%(tac, diameter, Q_cond, Q_reb, energy_cost, capital_cost))
+for i in range(model.N):
+    print("%d\tTemp: %3.3f\tPres: %3.3f"%(i, model.T_stage[i], model.P_stage[i]))
+    
