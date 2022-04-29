71d40d5e9497497a91aeb44c358ef77c70f7ad9e
Shanice Tan <shanice.tan@student.manchester.ac.uk>
2022-04-29 09:13:32 +0800

    Added seperate files for each case

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ model = m.Model (
     filepath = os.path.join(os.getcwd(), 'Simulation 5.bkp'), 
     N = 101,
     RR = 0.924,
+    distilate_rate = 69.2,
     tray_spacing = 0.63636,
     # Tray Type: SIEVE, CAPS
     tray_type="CAPS",
@@ -34,6 +35,44 @@ model.update_manipulated(
     tray_type = "SIEVE",
     hydraulics = True
 )
+
+model.update_manipulated(
+    P_cond = 1.013, 
+    P_drop_1 = 0.06,
+    P_drop_2 = 0.06,
+    RR = 0.9002, 
+    N = 50, 
+    feed_stage = 35, 
+    tray_spacing = 0.6364, 
+    tray_type = "SIEVE",
+    hydraulics = False
+)
+
+model.update_manipulated(
+    P_cond = 1.013, 
+    P_drop_1 = 0.01,
+    P_drop_2 = 0.01,
+    RR = 0.9348, 
+    N = 35, 
+    feed_stage = 25, 
+    tray_spacing = 0.1664, 
+    tray_type = "CAPS",
+    hydraulics = True
+)
+
+model.update_manipulated(
+    P_cond = 1.013, 
+    P_drop_1 = 0.06,
+    P_drop_2 = 0.06,
+    RR = 0.8985, 
+    N = 50, 
+    feed_stage = 35, 
+    tray_spacing = 0.6364, 
+    tray_type = "CAPS",
+    hydraulics = False
+)
+
+
 model.run()
 tac = model.TAC
 diameter = model.diameter
